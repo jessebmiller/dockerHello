fbf3b14eb7978c92ccd32e6d83870d73e918c71a
jesse B Miller <jmiller@adops.com>
2014-03-07 19:10:30 -0600

    last try

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 export DOCKER_HOST=tcp://localhost:4243
-docker -d &
+export DOCKER_HOST=tcp://localhost:4243 && docker -d &
 sleep 3
-docker build -t dockerHello /home/travis/build/jessebmiller/dockerHello
+export DOCKER_HOST=tcp://localhost:4243 && docker build -t dockerHello /home/travis/build/jessebmiller/dockerHello
